ace995daeefb125282c26ebdf6b11c827817d8ea
Mahlika Geore <mkg4541@rit.edu>
2020-12-09 23:30:18 -0500

    final push

--- Cleaning/cluster.py
+++ Cleaning/cluster.py
@@ -1,4 +1,4 @@
-#importing the required packages
+"""importing the required packages"""
 import pandas as pd
 from pylab import rcParams
 import seaborn as sb
@@ -10,35 +10,38 @@ import sklearn
 from sklearn.cluster import DBSCAN
 from collections import Counter
 
-#this function creates the clusters from DBSCAN
+"""this function creates the clusters from DBSCAN"""
 def creating_clusters(filename):
-    #creating a list for all the years that will be ran
+    """creating a list for all the years that will be ran"""
     Years = ['2007','2008','2009','2010','2011','2012','2013','2014','2015','2016']
     x = []
     y = []
     location = []
     colors = []
 
-    #this loop will run and print for each year individually
+    """this loop will run and print for each year individually"""
     for yr in Years:  
         df = pd.read_excel(filename, sheet_name = eval('yr'))
         table = df.iloc[:,[0,1,2,9,13,17]]
-        #removing any None values
+        """removing any None values"""
         data = df.iloc[:,[0,9,13,17]].dropna()
 
-        #creating the row numbers
+        """creating the row numbers"""
         data['row_num'] = np.arange(len(data))
 
-        #the average for all cities will be the sum of their clearance rates divided by three
-        #in order to do this, I'm using the row number to find the appropriate values since the indexs aren't are consecutive
+        """
+        the average for all cities will be the sum of their clearance rates divided by three
+        in order to do this, I'm using the row number to find the appropriate values since the indexs aren't are consecutive
+        """
+        
         avg = []
         for i in range(len(data)):
             avg.append(float((data.loc[data['row_num'] == i]['Murder/Nonnegligent Manslaughter Clearance Rate'] + data.loc[data['row_num'] == i]['Aggravated Assault Clearance Rate'] + data.loc[data['row_num'] == i]['Robbery Clearance Rate'])/3))
     
-        #the averages are saved as a new column
+        """the averages are saved as a new column"""
         data['average'] = avg
 
-        #The model for DBSCAN
+        """The model for DBSCAN"""
         model = DBSCAN(eps = 15000, min_samples=3).fit(data)
         print(yr + 's model is: ')
         print(model)
@@ -46,7 +49,7 @@ def creating_clusters(filename):
 
         outliers_df = pd.DataFrame(data)
      
-        #The groups of clusters
+        """The groups of clusters"""
         print(yr + 's grouping is: ')
         print(Counter(model.labels_))
         print()
@@ -56,11 +59,11 @@ def creating_clusters(filename):
         print()
 
 
-        #Printing each outlier one at a time
+        """Printing each outlier one at a time"""
         for i in outliers_df[model.labels_==-1]['Total Population']:
             print(t.loc[data['Total Population'] == i]['City'].to_string())
      
-        #Adding the x and y values and the colors to a list for the later plots
+        """Adding the x and y values and the colors to a list for the later plots"""
         colors.append(model.labels_)
         x.append(data.iloc[:,0])
         y.append(data.iloc[:,5])
--- Cleaning/networking.py
+++ Cleaning/networking.py
@@ -1,6 +1,8 @@
-#Mahlika George - Capstone Project
+"""
+Mahlika George - Capstone Project
 
-#importing the required packages
+importing the required packages
+"""
 import networkx as nx
 import math as m
 import matplotlib.pyplot as plt
@@ -8,7 +10,7 @@ import pandas as pd
 import numpy as np
 import random as ran
 
-#creating a function to calculate the euclidean distance of two given cities
+"""creating a function to calculate the euclidean distance of two given cities"""
 def euclideanDistance(a,b,G):
     diff1 = float(G.nodes[a]['A_CR']) - float(G.nodes[b]['A_CR'])
     diff2 = float(G.nodes[a]['M_CR']) - float(G.nodes[b]['M_CR'])
@@ -16,10 +18,10 @@ def euclideanDistance(a,b,G):
     distance = m.sqrt((diff1**2) + (diff2**2) + (diff3**2))
     return distance
     
-#creating a function that will create the networks
+"""creating a function that will create the networks"""
 def creating_networks(df):
 
-    #initilizaing the values
+    """initilizaing the values"""
     table = df.iloc[:,[0,1,2,9,13,17]].dropna()
     states = table.iloc[:,2]
     cities = table.iloc[:,1]
@@ -28,18 +30,18 @@ def creating_networks(df):
     Mclearance_rate = table.iloc[:,4]
     Rclearance_rate = table.iloc[:,5]
 
-    #creating the row numbers
+    """creating the row numbers"""
     table['row_num'] = np.arange(len(table))
 
 
-    #creating the graph in NetworkX
+    """creating the graph in NetworkX"""
     G=nx.Graph()
 
-    #Adding a node for each of the cities
+    """Adding a node for each of the cities"""
     for x in range(len(table)):
         G.add_node(x)
 
-    #storing the values each city will possess as a source of information
+    """storing the values each city will possess as a source of information"""
     for x in range (len(table)):
         G.nodes[x]['state'] = table.loc[table['row_num'] == x]['State']
         G.nodes[x]['city'] = table.loc[table['row_num'] == x]['City']
@@ -49,8 +51,9 @@ def creating_networks(df):
         G.nodes[x]['R_CR'] = table.loc[table['row_num'] == x]['Robbery Clearance Rate']
 
 
-    #running all possible combinations of nodes and calculating ther euclidean distance
-    #this loop also keeps track of the distances and the combinations of nodes
+    """running all possible combinations of nodes and calculating ther euclidean distance
+    this loop also keeps track of the distances and the combinations of nodes
+    """
     e_d = []
     combinations = []
     for i in range (len(table)):
@@ -58,33 +61,23 @@ def creating_networks(df):
             e_d.append(euclideanDistance(i,((len(table)-1)+j+1)-(len(table)-1),G))
             combinations.append([i,((len(table)-1)+j+1)-(len(table)-1)])
 
-    #setting the colors for the nodes        
-    #color_map = ['black'] * len(table)
+    """setting the colors for the nodes        
+    color_map = ['black'] * len(table)
+    """
     n = 1
     while (nx.number_connected_components(G)!=1):
         for x in range(len(e_d)):
             a = combinations[x][0]
             b = combinations[x][1]
 
-            #checking if the euclidean distance is less than 10
+            """checking if the euclidean distance is less than 10"""
             if e_d[x] <= n:
                 G.add_edge(a,b)
             
         n = n + 1
             
-    #splitting the edges into groups so they can be later colored accordingly
-    #low_edge_list = []
-    #med_edge_list = []
-    #high_edge_list = []
-    #for x in G.edges:
-        #if euclideanDistance(x[0],x[1],G)<=3:
-            #high_edge_list.append(x)
-        #if 3<euclideanDistance(x[0],x[1],G) and euclideanDistance(x[0],x[1],G)<=5:
-            #med_edge_list.append(x)
-        #if 5<euclideanDistance(x[0],x[1],G) and euclideanDistance(x[0],x[1],G)<=10:
-            #low_edge_list.append(x)
 
-    #labeling each city 
+    """labeling each city """
     labels = {i : cities.iloc[i] for i in range(0, len(cities))}
         
     return [G,labels]
